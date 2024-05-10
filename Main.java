87abe77ba3c761f603da5a5823e646038aa38111
Vladimir <buyanov.vv@yandex.ru>
2024-05-10 19:11:24 +0700

    изменил основную программу

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         String file = "input.txt";
-        HashMap<String, Integer> fruts = new HashMap<>();
         String[] arr = {};
         try {
             BufferedReader in = new BufferedReader(new FileReader(file));
@@ -14,26 +12,11 @@ public class Main {
             in.close();
         } catch (IOException e) {
             System.out.println("Невозможно прочитать файл " + file);
-            e.printStackTrace(); 
+            e.printStackTrace();
         }
-        for (int index = 0; index < arr.length; index++) {
-            if (fruts.containsKey(arr[index])) {
-                fruts.put(arr[index], fruts.get(arr[index])+1);
-            } else {fruts.put(arr[index], 1);}               
-        }
-        int count = 0;
-        String maxWordLength = "";
-        for (String frut : fruts.keySet()) {
-            count += fruts.get(frut);
-            if (frut.length()>maxWordLength.length()) {
-                maxWordLength = frut;
-            }
-            System.out.println("фрукт "+frut+" в количестве: "+fruts.get(frut)+" шт.");            
-        }
-        System.out.println("_____________________________");
-        System.out.println("Количество слов в файле: " + count);
-        System.out.println("_____________________________");
-        System.out.println("Самое длинное слово: " + maxWordLength);
-        System.out.println("_____________________________");
+        Basket basket = new Basket(arr);
+        System.out.println("Количество слов в файле: " + basket.AllFoodCount()+"\n");
+        System.out.println("Самое длинное слово: " + basket.MaxLengthFoodsName()+"\n");
+        System.out.println(basket);
     }
 }
